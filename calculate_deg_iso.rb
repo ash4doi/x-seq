2b309449b7998b5ba4cfb9b2dfb79ffafb3cac6f
ash4doi <ash4doi@gmail.com>
2023-01-24 15:12:46 +0900

    add scripts for isoform deg

---
+++ calculate_deg_iso.rb
@@ -0,0 +1,29 @@
+require 'erb'
+require '~/src/x-seq/calculate_deg.rb'
+
+class CalculateDegIso < CalculateDeg
+
+  private
+
+    def genes_results
+      @sample_ids.map { |id| "./RSEM/#{id}.isoforms.results" }.join(" ")
+    end
+
+    def edgeR_script
+      erb = ERB.new(IO.read("#{x_seq_dir}/edgeR_script_iso.r.erb"))
+      erb.result(binding)
+    end
+
+    def results_format_script
+      erb = ERB.new(IO.read("#{x_seq_dir}/results_format_iso.r.erb"))
+      erb.result(binding)
+    end
+end
+
+if __FILE__ == $0
+  calculate_deg = CalculateDegIso.new(ARGV[0])
+  calculate_deg.generate_matrix
+  calculate_deg.write_edgeR_script
+  calculate_deg.write_results_format_script
+  calculate_deg.run_scripts
+end
