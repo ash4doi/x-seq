b99f21322a761b232dc1387ea7a1f2bf696fe306
ash4doi <ash4doi@gmail.com>
2023-10-11 16:53:40 +0900

    add heatmap script for pathway genes

---
+++ make_heatmap_from_pathway.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+require 'optparse'
+
+class MakeHeatmapFromPathway
+
+  def create_heatmaps
+    write_heatmap_interesting_genes_r
+    system("R --no-save < ./heatmap_pathway_genes.r")
+  end
+
+  private
+
+    def x_seq_dir
+      "#{Dir.home}/src/x-seq"
+    end
+
+    def write_heatmap_interesting_genes_r
+      erb = ERB.new(IO.read("#{x_seq_dir}/heatmap_pathway_genes.r.erb"))
+      File.open("./heatmap_pathway_genes.r", "w") { |f| f.print erb.result(binding) }
+    end
+end
+
+if __FILE__ == $0
+  make_heatmap_from_list = MakeHeatmapFromPathway.new()
+  make_heatmap_from_list.create_heatmaps
+end
