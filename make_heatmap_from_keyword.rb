c9e22c7ab6dd99884d02f0a65be62fb0c0512fdb
ash4doi <ash4doi@gmail.com>
2023-11-08 17:20:42 +0900

    add methods to read filtering rule from file

--- make_heatmap_from_keyword.rb
+++ make_heatmap_from_keyword.rb
@@ -6,17 +6,27 @@ require 'optparse'
 class MakeHeatmapFromKeyword
 
   def initialize(argv)
-    @params = argv.getopts("r:k:", "result:all_results.tsv", "keyword:")
+    @params = argv.getopts("r:k:f:", "result:all_results.tsv", "keyword:", "filtering_rule:")
   end
 
   def extract_genes_from_keyword
-    write_select_keyword_genes_r
-    system("R --no-save < ./select_keyword_genes.r")
+    if keyword = @params["k"] || @params["keyword"]
+      write_select_keyword_genes_r
+      system("R --no-save < ./select_keyword_genes.r")
+    else
+      write_select_filtering_genes_r
+      system("R --no-save < ./select_filtering_genes.r")
+    end
   end
 
   def create_heatmaps
-    write_heatmap_keyword_genes_r
-    system("R --no-save < ./heatmap_keyword_genes.r")
+    if keyword = @params["k"] || @params["keyword"]
+      write_heatmap_keyword_genes_r
+      system("R --no-save < ./heatmap_keyword_genes.r")
+    else
+      write_heatmap_filtering_genes_r
+      system("R --no-save < ./heatmap_filtering_genes.r")
+    end
   end
 
   private
@@ -36,6 +46,22 @@ class MakeHeatmapFromKeyword
       keyword = @params["k"] || @params["keyword"]
       erb = ERB.new(IO.read("#{x_seq_dir}/heatmap_keyword_genes.r.erb"))
       File.open("./heatmap_keyword_genes.r", "w") { |f| f.print erb.result(binding) }
+    end
+
+    def write_select_filtering_genes_r
+      result_file = (@params["r"] || @params["result"])
+      filtering_file = (@params["f"] || @params["filtering_rule"])
+      filtering_name = File.basename(filtering_file, ".*")
+      filtering_rule = File.open(filtering_file, "r") {|f| f.read}
+      erb = ERB.new(IO.read("#{x_seq_dir}/select_filtering_genes.r.erb"))
+      File.open("./select_filtering_genes.r", "w") { |f| f.print erb.result(binding) }
+    end
+
+    def write_heatmap_filtering_genes_r
+      filtering_file = (@params["f"] || @params["filtering_rule"])
+      filtering_name = File.basename(filtering_file, ".*")
+      erb = ERB.new(IO.read("#{x_seq_dir}/heatmap_filtering_genes.r.erb"))
+      File.open("./heatmap_filtering_genes.r", "w") { |f| f.print erb.result(binding) }
     end
     
     def clean_name(keyword)
