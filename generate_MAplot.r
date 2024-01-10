4e9f8815be6c0453736bd35e404eed86d055e622
ash4doi <ash4doi@gmail.com>
2024-01-10 13:14:50 +0900

    add scripts for MA plot

---
+++ generate_MAplot.r
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(ggrepel)
+library(RColorBrewer)
+
+up_col       <- brewer.pal(9, "Reds")[4]
+down_col     <- brewer.pal(9, "Blues")[4]
+markup_col   <- brewer.pal(9, "Reds")[7]
+markdown_col <- brewer.pal(9, "Blues")[7]
+non_col      <- brewer.pal(9, "Greys")[3]
+mark_col     <- "black"
+
+create_MAplot_data <- function(de_results, gene_list, logfc_threshold){
+
+  colnames(de_results) <- colnames(de_results) %>% str_replace("comp.*_", "")
+
+  plot_data <- de_results %>%
+    left_join(gene_list) %>%
+    select(GeneSymbol, PValue, logFC, logCPM, marker) %>%
+    mutate("marker_symbol" = if_else(marker == "yes" ,
+                                     GeneSymbol, NA_character_)) %>%
+    mutate(deg = case_when(marker == "yes" & PValue < 0.05 & logFC >  logfc_threshold ~ "markup",
+                           marker == "yes" & PValue < 0.05 & logFC < -logfc_threshold ~ "markdown",
+                                             PValue < 0.05 & logFC >  logfc_threshold ~ "up",
+                                             PValue < 0.05 & logFC < -logfc_threshold ~ "down",
+                           marker == "yes"               ~ "mark",
+                                                   TRUE  ~ "none"))
+  return(plot_data)
+}
+
+create_MAplot <- function(plot_data, comp_name, logfc_threshold){
+  dir.create("./results", showWarnings = FALSE)
+
+  e <- ggplot(plot_data, aes(logCPM, logFC, label = marker_symbol)) +
+    geom_point(data = plot_data %>% filter(deg == "none"), color = non_col)  +
+    geom_point(data = plot_data %>% filter(deg == "mark"), color = mark_col) +
+    geom_point(data = plot_data %>% filter(deg == "up"),   color = up_col)   +
+    geom_point(data = plot_data %>% filter(deg == "down"), color = down_col) +
+    geom_point(data = plot_data %>% filter(deg == "markup"),   color = markup_col)   +
+    geom_point(data = plot_data %>% filter(deg == "markdown"), color = markdown_col) +
+    geom_text_repel(data = plot_data %>% filter(!is.na(marker_symbol)),
+                    max.overlaps = Inf, color = mark_col)
+
+  ee <- e + theme_light(base_size = 16) +
+    theme(legend.position = "none") +
+    scale_color_manual(values = c(down_col, mark_col, markdown_col, markup_col, non_col, up_col)) +
+    labs(title = comp_name)
+
+  plot_file <- paste("./results/MA_plot_", comp_name, ".png", sep = "")
+  ggsave(plot_file, width= 7, height = 7, unit = "in")
+}
---
+++ make_MAplot.rb
@@ -0,0 +1,55 @@
+
+#!/usr/bin/env ruby
+
+require 'erb'
+require 'optparse'
+
+class MakeMAplot
+
+  def initialize(argv)
+    params = argv.getopts("i:g:", "ident:GeneSymbol", "gene_list:gene_list.txt")
+    @uniq_id   = params["i"] || params["ident"]
+    @list_file = params["g"] || params["gene_list"]
+  end
+
+  def create_MAplot
+    write_MAplot_r
+    system("R --no-save < ./MAplot.r")
+  end
+
+  private
+
+    def x_seq_dir
+      "#{Dir.home}/src/x-seq"
+    end
+
+    def comp_list
+      comp = IO.readlines("./comp_list.txt").map(&:chomp)
+      comp.map.with_index(1) { |pair, i| [i, pair_to_array(pair)] }.to_h
+    end
+
+    def pair_to_array(pair)
+      pair.split(" vs ").map(&:strip)
+    end
+
+    def write_dummy_gene_list
+      if(!File.exist?("./gene_list.txt"))
+        File.open("./gene_list.txt", "w") do |f|
+          f.puts("GeneSymbol")
+        end
+      end
+    end
+
+    def write_MAplot_r
+      list_name = File.basename(@list_file, ".*")
+      write_dummy_gene_list unless File.exist?(@list_file)
+
+      erb = ERB.new(IO.read("#{x_seq_dir}/MAplot.r.erb"))
+      File.open("./MAplot.r", "w") { |f| f.print erb.result(binding) }
+    end
+end
+
+if __FILE__ == $0
+  make_MAplot = MakeMAplot.new(ARGV)
+  make_MAplot.create_MAplot
+end
