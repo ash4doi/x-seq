42c41eae912e13a50404c75ed2b5e83b3e38a5e4
ash4doi <ash4doi@gmail.com>
2024-02-20 16:10:04 +0900

    add -i option for isoform deg

--- calculate_deg_iso.rb
+++ calculate_deg_iso.rb
@@ -3,6 +3,12 @@ require '~/src/x-seq/calculate_deg.rb'
 
 class CalculateDegIso < CalculateDeg
 
+  def initialize(argv)
+    super
+    @params = argv.getopts("i:d:", "ident:TranscriptID", "dispersion:0.01")
+  end
+
+
   private
 
     def genes_results
@@ -16,13 +22,14 @@ class CalculateDegIso < CalculateDeg
 
     def results_format_script
       date_string = get_date_string
+      uniq_id = @params["i"] || @params["ident"]
       erb = ERB.new(IO.read("#{x_seq_dir}/results_format_iso.r.erb"))
       erb.result(binding)
     end
 end
 
 if __FILE__ == $0
-  calculate_deg = CalculateDegIso.new(ARGV[0])
+  calculate_deg = CalculateDegIso.new(ARGV)
   calculate_deg.generate_matrix
   calculate_deg.write_edgeR_script
   calculate_deg.write_results_format_script
